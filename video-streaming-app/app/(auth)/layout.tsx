351caa5c4c946d1401d0165a176dfed96aa5598f
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-10-18 09:34:47 +0530

    modify auth layout

---
+++ video-streaming-app/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function layout({children}:{children:React.ReactNode}) {
+  return (
+    <div className='h-full flex items-center justify-center bg-gray-800'>
+        {children}
+    </div>
+  )
+}
+
