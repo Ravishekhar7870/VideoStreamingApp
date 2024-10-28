499ba85e64dedb3515348748880c56fd83ba0a85
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-10-28 15:28:01 +0530

    add recommended in the sidebar

---
+++ video-streaming-app/app/(browse)/_component/Sidebar/Recommended.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+
+function Recommended() {
+    
+    return (
+        <div>Recommended Channels</div>
+    );
+}
+export default Recommended
--- video-streaming-app/app/(browse)/_component/Sidebar/index.tsx
+++ video-streaming-app/app/(browse)/_component/Sidebar/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Wrapper from './Wrapper'
 import Toggle from './Toggle'
+import Recommended from './Recommended'
 
 function Sidebar() {
+  
   return (
    <Wrapper>
      {/* <div className="hidden sm:block"> Hide Toggle on small screens */}
         <Toggle />
-    
+        <div className='space y-4 pt-4 lg:pt-0'>
+          <Recommended/>
+        </div>
    </Wrapper>
   )
 }
