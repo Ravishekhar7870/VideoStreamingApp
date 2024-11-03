465a0a1b2c692ff6e8aa0aaa869cee0e6b5586de
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-03 17:18:51 +0530

    update recommended service

--- video-streaming-app/lib/RecommendChannel.ts
+++ video-streaming-app/lib/RecommendChannel.ts
@@ -2,21 +2,57 @@ import DbConect from "./Dbconnect";
 import getUser from "./GetUser";
 import UserModel from "@/Model/User.mode";
 const RecommendChannel=async()=>{
-   try {
+  try {
     await  DbConect();
-    
-     const Users=await UserModel.aggregate(
-         [
-             {
-               $sort: {
-                 createdAt:1
-               }
-             }
-           ]
-     )
-     return Users
-   } catch (error:any) {
+  } catch (error:any) {
     throw new Error(error.message)
+  }
+  let currUser=null;
+   try {
+    currUser=await getUser()
+   } catch (error) {
+    currUser=null
    }
+  if(currUser){
+    try {
+    
+      const Users=await UserModel.aggregate(
+          [
+            {
+              $match: {
+                clerkId:{ $ne: 'user_2nq4xPw6NbEZVrHXcSJSBm3ZGas'}
+              }
+            },
+              {
+                $sort: {
+                  createdAt:1
+                }
+              }
+            ]
+      )
+      return Users
+    } catch (error:any) {
+     throw new Error(error.message)
+    }
+  }
+  else{
+    try {
+    
+    
+      const Users=await UserModel.aggregate(
+          [
+              {
+                $sort: {
+                  createdAt:1
+                }
+              }
+            ]
+      )
+      return Users
+    } catch (error:any) {
+     throw new Error(error.message)
+    }
+  }
+  
 }
 export default RecommendChannel
