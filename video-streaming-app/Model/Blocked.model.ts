9a1ed2e041d78fa34f3cb91ab5ce56bb7221e428
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-04 19:02:18 +0530

    add Blocked.model.ts

---
+++ video-streaming-app/Model/Blocked.model.ts
@@ -0,0 +1,23 @@
+import mongoose,{Schema,Document,Types} from "mongoose";
+interface Blocked extends Document{
+    _id:string
+    BlockedUserId:Types.ObjectId
+    BlockerUserId:Types.ObjectId
+}
+const BlockedSchema:Schema<Blocked>=new Schema({
+     BlockedUserId:{
+       type:Schema.Types.ObjectId,
+       ref:'User',
+       required:true
+     },
+     BlockerUserId:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+     }
+},
+{
+    timestamps:true
+})
+const BlockedModel=(mongoose.models.Blocked as mongoose.Model<Blocked>) || mongoose.model<Blocked>('Blocked',BlockedSchema) 
+export default BlockedModel
