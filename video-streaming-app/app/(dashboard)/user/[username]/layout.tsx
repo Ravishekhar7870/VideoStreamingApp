aafd8f14264c59de33dec1fc1dd6caa652fa2257
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-07 19:44:34 +0530

    add Siderbar.tsx to user/username and modify layout.tsx

--- video-streaming-app/app/(dashboard)/user/[username]/layout.tsx
+++ video-streaming-app/app/(dashboard)/user/[username]/layout.tsx
@@ -2,6 +2,7 @@ import { getUserbyUsername } from '@/lib/GetUser'
 import { redirect } from 'next/navigation'
 import React from 'react'
 import Navbar from './_component/Navbar/Navbar'
+import Sidebar from './_component/Sidebar/Sidebar'
 interface layoutProps{
     params:{username:string}
     children:React.ReactNode
@@ -15,6 +16,7 @@ async function layout({children,params}:layoutProps) {
     <>
        <Navbar/>
        <div className='flex h-full pt-20'>
+        <Sidebar/>
         {children}
         </div>
         </>
