433b74430acc169e687f8767b7063a783b8d0b33
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-10 17:43:31 +0530

    update ingress.actions.ts

--- video-streaming-app/Actions/Ingress.actions.ts
+++ video-streaming-app/Actions/Ingress.actions.ts
@@ -1,12 +1,16 @@
 'use server'
 import getUser from '@/lib/GetUser'
+import StreamModel from '@/Model/Stream.model'
 import {IngressAudioEncodingPreset,IngressInput,IngressClient,IngressVideoEncodingPreset,RoomServiceClient, type CreateIngressOptions}  from 'livekit-server-sdk'
 import { TrackSource } from 'livekit-server-sdk'
+import { revalidatePath } from 'next/cache'
+import { userAgent } from 'next/server'
 const roomService=new RoomServiceClient(
     process.env.LIVEKIT_API_URL!,
     process.env.LIVEKIT_API_KEY!,
     process.env.LIVEKIT_SECRET_KEY!
 )
+const ingressclient=new IngressClient(process.env.LIVEKIT_API_URL!)
 const CreateIngress=async(ingressType:IngressInput)=>{
    const user=await getUser();
    if(!user){
@@ -25,6 +29,27 @@ const CreateIngress=async(ingressType:IngressInput)=>{
     options.video={
         source:TrackSource.CAMERA,
         preset:IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS,
-    } as any
+    } as any;
+    options.audio={
+      source:TrackSource.MICROPHONE,
+      preset:IngressAudioEncodingPreset.OPUS_STEREO_96KBPS
+    } as any;
+
   }
+  const ingress=await ingressclient.createIngress(ingressType,options)
+  if(!ingress || !ingress.url || !ingress.streamKey){
+    throw new  Error("failed to create ingress")
+  }
+  const userStream=await StreamModel.findOne({
+      UserId:user?._id
+  })
+  if(!userStream){
+    throw new  Error("no Stream found")
+  }
+  userStream.ingressId=ingress.ingressId
+  userStream.serverUrl=ingress.url
+  userStream.serverKey=ingress.streamKey
+ await userStream.save({validateBeforeSave:false})
+ revalidatePath(`/user/${user.username}/Keys`)
+ return ingress
 }
