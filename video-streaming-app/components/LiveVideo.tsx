4dac8c1235852e0173623fe41b1c49badd49b3a4
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-12 15:52:12 +0530

    modify LiveVideo.tsx and update package.json

--- video-streaming-app/components/LiveVideo.tsx
+++ video-streaming-app/components/LiveVideo.tsx
@@ -1,26 +1,43 @@
 'use client'
 import { Participant ,Track} from 'livekit-client'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useTracks } from '@livekit/components-react'
 import FullScreenControll from './FullScreenControll'
+import {useEventListener} from 'usehooks-ts'
 interface LiveVideoProps{
     Participants:Participant
 }
 function LiveVideo({Participants}:LiveVideoProps) {
     const VideoRef=useRef<HTMLVideoElement>(null)
     const DivRef=useRef<HTMLDivElement>(null)
+    const [isFullScreen,setIsFullScreen]=useState(false)
     useTracks([Track.Source.Camera,Track.Source.Microphone]).filter((track)=>track.participant.identity===Participants.identity)
     .forEach((track)=>{
         if(VideoRef.current){
             track.publication.track?.attach(VideoRef.current)
         }
     })
+    const onToggle=()=>{
+      if(isFullScreen){
+        document.exitFullscreen();
+        setIsFullScreen(false);
+      }
+      else if(DivRef?.current){
+        DivRef?.current?.requestFullscreen()
+        setIsFullScreen(true)
+      }
+    }
+    const handleFullScreenChange=()=>{
+     const  isCurrentlyFullScreen=document.fullscreenElement!== null
+     setIsFullScreen(isCurrentlyFullScreen)
+    }
+    useEventListener("fullscreenchange",handleFullScreenChange,DivRef)
   return (
     <div className='relative h-full flex' ref={DivRef}>
      <video ref={VideoRef} width='100%'/>
      <div className='absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all'>
       <div className='absolute bottom-0 flex w-full h-14 item-center justify-between bg-gradient-to-r from-neutral-900 px-4'>
-      <FullScreenControll isFullScreen={false} onToggle={()=>{}}/>
+      <FullScreenControll isFullScreen={isFullScreen} onToggle={onToggle}/>
       </div>
      </div>
     </div>
