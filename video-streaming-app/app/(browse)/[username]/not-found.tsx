28ffa6f4725a426c01af43fca6817288708604bc
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-22 23:17:07 +0530

    add not-found.tsx

---
+++ video-streaming-app/app/(browse)/[username]/not-found.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+function notFound() {
+  return (
+    <div className='h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground'>
+        <h1 className='text-4xl'>
+            404
+        </h1>
+        <p>
+            Couldn't find the user
+        </p>
+        <Button asChild variant='primary'>
+            <Link href='/'>
+            GO BACK
+            </Link>
+        </Button>
+
+    </div>
+  )
+}
+
+export default notFound
