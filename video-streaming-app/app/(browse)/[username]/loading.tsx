b526b69875fe3d06117a379308d90bc809b3d676
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-22 23:02:09 +0530

    add loading.tsx

---
+++ video-streaming-app/app/(browse)/[username]/loading.tsx
@@ -0,0 +1,12 @@
+import { StreamPlayerSkelton } from '@/app/(StreamPlayer)/StreamPlayer'
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='h-full'>
+        <StreamPlayerSkelton/>
+    </div>
+  )
+}
+
+export default loading
