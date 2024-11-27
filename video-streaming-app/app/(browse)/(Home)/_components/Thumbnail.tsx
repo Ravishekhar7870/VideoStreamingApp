62f44554d0c88491cb4411a33643c94033366a62
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-27 23:28:45 +0530

    modify Thumbnail.tsx to add more ui to it

--- video-streaming-app/app/(browse)/(Home)/_components/Thumbnail.tsx
+++ video-streaming-app/app/(browse)/(Home)/_components/Thumbnail.tsx
@@ -1,3 +1,6 @@
+import ChannelAvatar from '@/components/Avatar';
+import { Avatar } from '@/components/ui/avatar';
+import Image from 'next/image';
 import React from 'react'
 interface ThumbnailProps{
     src?:string,
@@ -6,8 +9,29 @@ interface ThumbnailProps{
       isLive?:boolean
 }
 function Thumbnail({src,fallback,username,isLive}:ThumbnailProps) {
+    let ImageToShow;
+    if(!src){
+        ImageToShow=(
+            <div className='bg-background flex flex-col items-center justify-center gap-y-4 h-full w-full transition-transform
+            group-hover:translate-x-2 group-hover:-translate-y-1 rounded-md'>
+             <ChannelAvatar username={username} islive={isLive} imageUrl={fallback} />
+            </div>
+        )
+    }
+    else{
+      ImageToShow=(
+        <Image src={src} fill alt='Thumbnail' className='object-cover transition-transform group-hover:translate-x-2
+        group-hover:-translte-y-2 rounded md'
+         />
+      )
+    }
   return (
-    <div>Thumbnail</div>
+    <div className='group aspect-video relative rounded-md cursor-pointer'>
+        <div className='rounded-md absolute inset-0 bg-blue-500 opacity-0 group-hover:opacity-100 transition-opacity flex items-center justify-center'>
+         
+        </div>
+        {ImageToShow}
+    </div>
   )
 }
 
