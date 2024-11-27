8148f0eed29e27f6dad3fc4d8eb2401e453b9a07
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-27 20:28:45 +0530

    modify Streamcard.tsx to enable redirecting to user live Streams

--- video-streaming-app/app/(browse)/(Home)/_components/Streamcard.tsx
+++ video-streaming-app/app/(browse)/(Home)/_components/Streamcard.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Stream } from '@/Model/Stream.model'
 import { User } from '@/Model/User.mode'
+import Link from 'next/link'
 interface StreamCardwithUser  extends Stream{
    User:User,
    Blocked?:Array<any>
 }
 interface StreamcardProps{
-    data:StreamCardwithUser[]
+    data:StreamCardwithUser
 }
 function Streamcard({data}:StreamcardProps) {
   return (
-    <div>Streamcard</div>
+    <Link href={`/${data.User.username}`}>
+   <div className='h-full w-full space-y-4'>
+    Stream card
+   </div>
+   </Link>
   )
 }
 
