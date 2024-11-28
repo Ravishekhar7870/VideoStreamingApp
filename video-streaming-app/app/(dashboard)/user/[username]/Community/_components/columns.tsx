be5be53b7fa39abf5c4130a136d34ec9d01ae557
Ravi Shekhar <yadavravishekhar01@gmail.com>
2024-11-29 00:25:44 +0530

    update columns.tsx

--- video-streaming-app/app/(dashboard)/user/[username]/Community/_components/columns.tsx
+++ video-streaming-app/app/(dashboard)/user/[username]/Community/_components/columns.tsx
@@ -18,7 +18,7 @@ export const columns: ColumnDef<BlockedUser>[] = [
     accessorKey: "username",
     header: ({column})=>{
         return (
-        <Button variant='ghost' onClick={()=> column.toggleSorting(column.getIsSorted==="asc")}>
+        <Button variant='ghost' onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
           username
           <ArrowUpDown className="ml-2 h-4 w-4"/>
         </Button>
